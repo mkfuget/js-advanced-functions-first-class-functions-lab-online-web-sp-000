3d7460e5daf1d3ed1eff7fff714de039b527f3f9
Michael Fuget <michaelfuget@gmail.com>
2021-02-14 05:41:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const returnFirstTwoDrivers = function(funcArray){
   return [funcArray[0], funcArray[1]];
 }
-const returLastTwoDrivers = function(funcArray){
+const returnLastTwoDrivers = function(funcArray){
   return [funcArray[funcArray.length-1], funcArray[funcArray.length-2]];
 }
